dcfa012c6a1ab7ee50a53549daedd8442cc7ad51
Nicolò Carandini <n.carandini@outlook.com>
2018-12-12 01:13:57 +0100

    Add solution

---
+++ src/TPCWare.XmasDev.Models/XmasCard.cs
@@ -0,0 +1,20 @@
+﻿namespace TPCWare.XmasDev.Models
+{
+    public class XmasCard
+    {
+        public XmasCard() { }
+
+        public XmasCard(int cardId, string title, string description, string url)
+            => (CardId, Title, Description, Url) = (cardId, title, description, url);
+
+        public int CardId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Url { get; set; }
+
+        public override string ToString() => Title;
+    }
+}
